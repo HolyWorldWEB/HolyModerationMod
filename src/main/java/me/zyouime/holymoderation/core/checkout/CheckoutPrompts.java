51c5ed26ae9b2666c46ac5a5d078e45d0e3b8333
zyouime <igorneizvestny69@gmail.com>
2026-09-04 11:50:30 +0300

    Убрать ник из подставляемых команд завершения

--- src/main/java/me/zyouime/holymoderation/core/checkout/CheckoutPrompts.java
+++ src/main/java/me/zyouime/holymoderation/core/checkout/CheckoutPrompts.java
@@ -5,6 +5,7 @@ import me.zyouime.holymoderation.core.util.Colors;
 import net.minecraft.text.MutableText;
 
 public record CheckoutPrompts(ChatService chatService) {
+
     public void showJournalPrompt(String suspect) {
         for (CheckoutReason reason : CheckoutReason.values()) {
             send("Внести проверку: %s".formatted(reason.getLabel()),
@@ -13,7 +14,7 @@ public record CheckoutPrompts(ChatService chatService) {
         }
     }
 
-    public void showEndPrompt(String suspect) {
+    public void showEndPrompt() {
         for (CheckoutResult result : CheckoutResult.values()) {
             if (!result.isNeedsStashChoice()) {
                 send("Закончить: %s".formatted(result.getLabel()),
@@ -21,28 +22,27 @@ public record CheckoutPrompts(ChatService chatService) {
                         "/hm endcheckout %s".formatted(result.getApiValue()));
                 continue;
             }
-            sendStashOptions(suspect, result);
+            sendStashOptions(result);
         }
     }
 
-    public void showBanReasonPrompt(String suspect, boolean destroyStash) {
+    public void showBanReasonPrompt(boolean destroyStash) {
         send("Дописать причину бана вручную",
                 "Нажмите, чтобы подставить команду и дописать причину",
-                "/hm endcheckout ban %s %s ".formatted(suspect, destroyStash));
+                "/hm endcheckout ban %s ".formatted(destroyStash));
     }
 
-    private void sendStashOptions(String suspect, CheckoutResult result) {
+    private void sendStashOptions(CheckoutResult result) {
         send("Закончить: %s + снести стеш".formatted(result.getLabel()),
                 "Нажмите, чтобы закончить проверку со сносом стеша",
-                "/hm endcheckout %s %s true".formatted(result.getApiValue(), suspect));
+                "/hm endcheckout %s true".formatted(result.getApiValue()));
         send("Закончить: %s + не сносить стеш".formatted(result.getLabel()),
                 "Нажмите, чтобы закончить проверку без сноса стеша",
-                "/hm endcheckout %s %s false".formatted(result.getApiValue(), suspect));
+                "/hm endcheckout %s false".formatted(result.getApiValue()));
     }
 
     private void send(String label, String hint, String command) {
-        MutableText component = chatService.suggestTextComponent(
-                "%s%s%s".formatted(Colors.AQUA, Colors.BOLD, label), hint, command);
+        MutableText component = chatService.suggestTextComponent("%s%s%s".formatted(Colors.AQUA, Colors.BOLD, label), hint, command);
         chatService.clientMessage(component);
     }
 }
